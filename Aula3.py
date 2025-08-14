1896286c411c0cd8d6f5702e6c06158e26fda0b7
curupira12 <davi.bayfox@gmail.com>
2025-08-14 20:41:33 -0300

    Adicionando calculadora2.py

---
+++ Aula3.py
@@ -0,0 +1,12 @@
+print("*** calculadira ***")
+numero1 = int(input("Digite o primeiro numero:"))
+numero2 = int(input("Digite o segundo numero:"))
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+print("Soma é:", soma)
+print("Subtração é:", subtracao)
+print("Multiplicação é:", multiplicacao)
+print("Divisão é:", divisao)
+print("*** Fim do programa ***")
---
+++ Aula3_calculadora.py
@@ -0,0 +1,18 @@
+# calculadora - v2.0
+print("*** Calculadora ***")
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("A soma é:", soma)
+print("A subtração é:", subtracao)
+print("A multiplicação é:", multiplicacao)
+print("A divisão é:", divisao)
+
+print("*** Fim do programa ***")
+
