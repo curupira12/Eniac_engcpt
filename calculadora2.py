f39daed464551b80899acd57418acfe3452e9751
curupira12 <davi.bayfox@gmail.com>
2025-08-13 21:47:38 -0300

    Adicionando calculadora2.py

---
+++ calculadora2.py
@@ -0,0 +1,18 @@
+# programa 5 - Calculadora - Algoritmo
+print("Calculadora")
+
+# Entrada de números
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+# Operações matemáticas
+Soma = numero1 + numero2
+Subtracao = numero1 - numero2
+Multiplicacao = numero1 * numero2
+Divisao = numero1 / numero2
+
+# Saída dos resultados
+print("A soma é:", Soma)
+print("A subtração é:", Subtracao)
+print("A multiplicação é:", Multiplicacao)
+print("A divisão é:", Divisao)
